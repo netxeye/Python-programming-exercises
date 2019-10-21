bff320026a47d1f869028fe7bc964ab1fcb0717f
tao lu <netxeye@gmail.com>
2019-10-20 18:10:32 -0700

    sequence generator requires more practices

---
+++ q7.py
@@ -0,0 +1,11 @@
+#! /bin/env python3
+# -*- coding: utf-8 -*-
+
+def two_dimension_array(x,y):
+    two_dimension = [[0 for y_value in range(y)] for x_value in range(x)]
+    for x_value in range(x):
+        for y_value in range(y):
+            two_dimension[x_value][y_value] = x_value * y_value
+    return two_dimension
+
+print(two_dimension_array(3,5))
