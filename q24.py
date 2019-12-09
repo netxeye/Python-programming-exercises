c5352d396f63e98393c446c8a106a1742d2d1c8d
tao.lu <netxeye@gmail.com>
2019-12-08 19:10:37 -0800

    update

---
+++ q24.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def question24_doc():
+    '''
+    Python has may built-in functinos, and if you do not know how to use it,
+    you can read document online or find some books. But Python has a built-in
+    document function for every built-in functinons.
+    I write a progeam to print out int.__doc__, input.__doc__ and abs.__doc__
+    '''
+    print(abs.__doc__)
+    print(input.__doc__)
+    print(int.__doc__)
+
+
+question24_doc()
