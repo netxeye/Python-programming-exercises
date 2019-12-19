d22840f16321481d8a9ed75552a49b006c3f7948
tao.lu <netxeye@gmail.com>
2019-12-19 08:01:18 -0800

    update

--- answers/q32.py
+++ answers/q32.py
@@ -3,4 +3,6 @@
 
 
 def questin32_print_key(dictionary):
+    if (1 in dictionary.keys()) or (2 in dictionary.keys()) or (3 in dictionary.keys()):
+        raise ValueError('Exception: The dictionary has to contain 1, 2 or 3')
 
